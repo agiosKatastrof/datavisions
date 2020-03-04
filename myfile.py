19a25e6e5864db4537ca4c39c12c61470fe45e5b
agioskatastrof <joecho@gmail.com>
2020-03-04 15:57:28 -0500

    removed myfile.py

--- myfile.py
+++
@@ -1,6 +0,0 @@
-def pickCompany(company,df):
-    dfC = df.loc[df['Company'] == company]
-    dfC_dates = dfC.groupby('Date').count()[['Complaint ID']]
-    dfC_dates = dfC_dates.reset_index()
-    dfC_dates = dfC_dates.rename(columns={'Complaint ID':company}) 
-    return dfC_dates
