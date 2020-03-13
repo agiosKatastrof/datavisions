26353c1a0ada2db6e5513381b059e852be2c427f
agioskatastrof <joecho@gmail.com>
2020-03-12 21:51:02 -0400

    init run

--- covid19/run.py
+++ covid19/run.py
@@ -1,23 +1,20 @@
 #!/usr/bin/env python
-#import sys
-#sys.path.append("/home/joe/datavisions/covid19")
+
 from covid19 import findFilesToProcess, parsePdf, findFilesToProcess, processFile
 
-#mypath = '/home/joe/data/covid19/who'
-mypath = '/home/joe/data/covid19/who/test'
+mypath = '/home/joe/data/covid19/who'   #where the pdfs are
 #mypath = '/run/user/1000/gvfs/smb-share:server=majesty.local,share=filesjoe/data/who_covid19_reports/renamed'
-filelist = '/home/joe/datavisions/covid19/filelist'
-summary = '/home/joe/data/covid19/summary'
-#dill.dump_session('data.db')
-#dill.load_session('data.db')
+filelist = '/home/joe/data/covid19/filelist'  # list of processed files
+summary = '/home/joe/data/covid19/summary' # summary data
 
-#dfToProcess = findFilesToProcess(mypath,filelist)
-#rows, columns = dfToProcess.shape
+dfToProcess = findFilesToProcess(mypath,filelist)
+rows, columns = dfToProcess.shape
 
-#if (rows > 0): #there are files to process
+if (rows > 0): #there are files to process
     #f = dfToProcess['file'].iloc[0]  # just do one file
-    
-#    for f in dfToProcess['file'].tolist():   
-#        print("found to process: ", f)
- #       out = processFile(mypath,filelist,summary,f)
- #       print("output: ", out) 
+    for f in dfToProcess['file'].tolist():   
+        print("found to process: ", f)
+        out = processFile(mypath,filelist,summary,f)
+        print("output: ", out) 
+
+exit(0)
