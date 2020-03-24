2302ab42d6f1b0508225cc4c43442c8165748d7b
agioskatastrof <joecho@gmail.com>
2020-03-23 23:51:42 -0400

    initial

--- aws/aws.py
+++ aws/aws.py
@@ -3,11 +3,14 @@
 import sys, getopt
 
 from cloudwatch import getMetrics
+id = 'i-0ca364ec6656ff517'
+
 
 def main(argv):
 
     usage = 'run.py -r -d <datadir> -f <filelist> -s <summary>'
-    getMetrics()
+    response = getMetrics(id)
+    print(response)
 
 if __name__ == "__main__":
     main(sys.argv)
--- aws/cloudwatch.py
+++ aws/cloudwatch.py
@@ -1,5 +1,26 @@
 import boto3
+import datetime
 
-def getMetrics():
+def getMetrics(InstanceId):
     print('get metrics')
-    return
+    cw = boto3.client('cloudwatch')
+
+    response = cw.get_metric_statistics(
+        Namespace='AWS/EC2',
+        MetricName='CPUUtilization',
+        Dimensions=[
+            {
+                'Name': 'InstanceId',
+                'Value': InstanceId
+            },
+        ],
+        StartTime='2020-03-22T23:23:00',
+        EndTime='2020-03-23T23:23:00',
+        Period=360,
+        Statistics=[
+            'Maximum',
+        ],
+        Unit='Percent'
+)
+
+    return response
