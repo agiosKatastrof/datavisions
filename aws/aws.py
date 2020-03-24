4f4b2ec8e7a0fb75c504bf1b20a566c5b47da14f
agioskatastrof <joecho@gmail.com>
2020-03-24 00:27:45 -0400

    parsed output

--- aws/aws.py
+++ aws/aws.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import sys, getopt
+import json
 
 from cloudwatch import getMetrics
 instanceId = 'i-0ca364ec6656ff517'
@@ -12,7 +13,14 @@ endt = '2020-03-23T23:23:00'
 
 def main(argv):
     response = getMetrics(instanceId,metric,namespace,startt,endt)
-    print(response)
+    #print(response['Datapoints'])
+    for item in response['Datapoints']:
+
+        print(item['Timestamp'],end=",")
+        print(instanceId,end=",")
+        print(item['Maximum'])
+
+
 
 if __name__ == "__main__":
     main(sys.argv)
