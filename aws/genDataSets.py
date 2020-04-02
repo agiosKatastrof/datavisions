f617b2bcb711378d0ecc411529217c718c3d2d5c
agioskatastrof <joecho@gmail.com>
2020-04-01 23:01:58 -0400

    dfs

--- aws/genDataSets.py
+++ aws/genDataSets.py
@@ -10,7 +10,7 @@ from gendata import genCurve
 from datetime import datetime, timedelta, date
 
 def main(argv):
-    usage = 'aws.py -s <seed> -x <max> -m <min> -i <inc> -n <n>'
+    usage = 'aws.py -s <seed> -x <max> -m <min> -i <inc>'
 
     instances = ['i-0ca364ec6656ff517','i-089389afc8097dac4','i-09f0182499b5eaf27']
     startt = '03/01/2020 00:00:00'
@@ -21,7 +21,7 @@ def main(argv):
 
 
     try:
-        opts, args = getopt.getopt(argv,"s:x:m:i:n:")
+        opts, args = getopt.getopt(argv,"x:m:i:n:")
     except:
         print(usage)
         sys.exit(2)
@@ -29,27 +29,31 @@ def main(argv):
     for opt, arg in opts:
         if opt == '-h':
             print(usage)
-            sys.exit()
-        elif opt == '-s':
-            start = float(arg)              
+            sys.exit()            
         elif opt == '-x':
             maxV = float(arg)
         elif opt == '-m':
             minV = float(arg)
         elif opt == '-i':
             inc = float(arg)
-        elif opt == '-n':
-            n = int(arg)
     
-    
+    times = []
     delta = timedelta(minutes=incMin)
     while startt <= endt:
-        print(startt)
+        times.append(startt)
         startt += delta
 
-  
-    out = genCurve(start,maxV,minV,inc,n)   
-    print(out) 
+    n = len(times)
+    out1 = genCurve(0.9,maxV,minV,inc,n)   
+    out2 = genCurve(0.8,maxV,minV,inc,n)   
+    out3 = genCurve(0.1,maxV,minV,inc,n)   
+
+
+    data = {'datetime':times, instances[0]:out1, instances[1]:out2, instances[2]:out3}
+    df = pd.DataFrame(data)
+    print(df.head(10))
+    print(df.describe())
+    print(df.info)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
