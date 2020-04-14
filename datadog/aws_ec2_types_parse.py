35b7929a3ed1dc6734cd3d359c1f32ef12af3c25
agioskatastrof <joecho@gmail.com>
2020-04-14 10:37:17 -0400

    seems to be final

--- datadog/aws_ec2_types_parse.py
+++ datadog/aws_ec2_types_parse.py
@@ -4,6 +4,7 @@ with open('/home/joe/data/aws/ec2_types.json') as f:
   data = json.load(f)
 
 for InstanceType in data["InstanceTypes"]:
-    print(InstanceType, "\n")
-    print(InstanceType["VCpuInfo"]["DefaultVCpus"], "\n")
-    print(InstanceType["MemoryInfo"]['SizeInMiB'], "\n")
+    print(InstanceType["InstanceType"],",",end='')
+    print(InstanceType["VCpuInfo"]["DefaultVCpus"],",",end='')
+    print(InstanceType["MemoryInfo"]['SizeInMiB'])
+  
